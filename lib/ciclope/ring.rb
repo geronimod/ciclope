0a61c71d6a7410d290cd83d820d15b4eafc36fac
Geronimo Diaz <geronimod@gmail.com>
2011-12-27 08:56:44 -0300

    Fix adapter condition in ring

--- lib/ciclope/ring.rb
+++ lib/ciclope/ring.rb
@@ -13,16 +13,15 @@ module Ciclope
       options ||= {}
       connections = Array(args).flatten.compact.uniq
       
-      raise NotEnoughConnections if connections.size < 2
-      
       @hosts = []
 
       connections.each do |c|
         config = Rails.configuration.database_configuration[c.to_s]
-        next unless config && config['adapter'] != 'mysql'
+        next unless config && config['adapter'] == 'mysql'
         @hosts << Host.new(ActiveRecord::Base.mysql_connection(config))
       end
 
+      raise NotEnoughConnections if connections.size < 2
       @hosts = @hosts.sort if options[:sort_hosts] && @hosts.count > 2
     end
     
